f6027223aac17f38ab99f00062c560f9c567076d
Uvexer <125573358+Uvexer@users.noreply.github.com>
2024-08-28 12:31:52 +0300

    Initial Commit

---
+++ ArtBadge/ArtBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtBadgeApp.swift
+//  ArtBadge
+//
+//  Created by bodya on 28.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ArtBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
